0ab820d325799697af5728fc975656fb14387e62
Cyril Pierron <cype59@gmail.com>
2021-10-08 16:36:58 +0200

    update env variable

--- components/DisqusComments.tsx
+++ components/DisqusComments.tsx
@@ -1,10 +1,9 @@
 import { DiscussionEmbed } from "disqus-react"
-import config from "../../config.json"
 
 const DisqusComments = ({ article }) => {
   const disqusShortname = "followmytripcomment"
   const disqusConfig = {
-    url: `${config.FRONT_URL}/article/${article.slug}`,
+    url: `http://localhost:3000/article/${article.slug}`,
     identifier: article.slug,
     title: article.title,
   }
