688965e0f36d714a071adc2be9f98579e5a5f7af
cype59 <cype59@gmail.com>
2021-10-13 11:48:41 +0200

    StrapiUrl according to the environement

--- lib/api.tsx
+++ lib/api.tsx
@@ -1,5 +1,9 @@
 export function getStrapiURL(path = "") {
-  return `${process.env.NEXT_PUBLIC_STRAPI_API_URL}${path}`
+  if (`${process.env.NODE_ENV}` === "production") {
+    return `${process.env.NEXT_PUBLIC_STRAPI_API_URL}${path}`
+  } else {
+    return `http://localhost:1337${path}`
+  }
 }
 
 // Helper to make GET requests to Strapi
