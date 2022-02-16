f8275a01b832ff628c371ab48ab21d70940b14e3
cype59 <cype59@gmail.com>
2022-02-16 20:19:29 +0100

    feat: update sitemap for vercel

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -10,12 +10,7 @@ export const getServerSideProps = async ({ res }) => {
   }[process.env.NODE_ENV]
 
   const staticPages = fs
-    .readdirSync(
-      {
-        development: "pages",
-        production: "./",
-      }[process.env.NODE_ENV]
-    )
+    .readdirSync("pages")
     .filter((staticPage) => {
       return ![
         "article",
