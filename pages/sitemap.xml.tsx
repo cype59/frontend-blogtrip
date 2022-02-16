eb5197f5d07f79524ffe3050d30f8e4175707b8e
cype59 <cype59@gmail.com>
2022-02-16 20:26:11 +0100

    fix: update sitemap on vercel

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -13,7 +13,7 @@ export const getServerSideProps = async ({ res }) => {
     .readdirSync(
       {
         development: "pages",
-        production: "./pages",
+        production: "./",
       }[process.env.NODE_ENV]
     )
     .filter((staticPage) => {
